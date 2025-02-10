63fa01d3bc35c8695af22426b4f0de1be2e017a0
Mickael Gaillard <mickael.gaillard@platform.sh>
2025-02-10 12:59:56 +0100

    Add more task/launch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 // Init Application.
+import { getCurrentInstance } from 'vue';
 import LocalStorage from '@/stores/localStorage';
 new LocalStorage().checkSchemaVersion();
 
+const app = getCurrentInstance()!.appContext.app
+app.config.globalProperties.toast_duration = 5000
 </script>
 
 <template>
