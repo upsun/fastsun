16f2e7fc35b7ef296f94d6acee4cb9491b93f6cc
Mickael Gaillard <mickael.gaillard@platform.sh>
2025-07-04 15:21:48 +0200

    Add more metrics #2

--- src/components/project/StatCard.vue
+++ src/components/project/StatCard.vue
@@ -5,6 +5,7 @@ import Chart from 'primevue/chart';
 import ProjectAPIService from './project.service';
 import 'chartjs-adapter-date-fns';
 import LocalStore from '@/stores/localStorage';
+import { error } from 'console';
 
 // Init
 const localStore = new LocalStore()
@@ -30,19 +31,90 @@ const chartData = {
       label: 'Request',
       data: [],
       fill: true,
-      borderColor: '#42A5F5',
+      borderColor: '#2196F3',
+      backgroundColor: '#2196F320',
       tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_cnt'
+    },
+    {
+      label: 'Hit',
+      data: [],
+      fill: true,
+      borderColor: '#4CAF50',
+      backgroundColor: '#4CAF5020',
+      tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_cnt'
+    },
+    {
+      label: 'Error',
+      data: [],
+      fill: true,
+      borderColor: '#F44336',
+      backgroundColor: '#F4433620',
+      tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_cnt'
+    },
+    {
+      label: 'Origin offload',
+      data: [],
+      fill: true,
+      borderColor: '#FF7043',
+      backgroundColor: '#FF704320',
+      tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_per'
+    },
+    {
+      label: 'Hit ratio',
+      data: [],
+      fill: true,
+      borderColor: '#9C27B0',
+      backgroundColor: '#9C27B020',
+      tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_per'
+    },
+    {
+      label: 'Cache Coverage',
+      data: [],
+      fill: true,
+      borderColor: '#00BCD4',
+      backgroundColor: '#00BCD420',
+      tension: 0.01,
+      borderWidth: 1,
+      yAxisID: 'y_per'
     },
   ],
 };
 const chartOptions = ref({
   animation: false,
   scales: {
-    y: {
+    y_cnt: {
+      type: 'linear',
+      display: true,
+      position: 'left',
       min: 0,
       suggestedMax: 2,
       ticks: {
         stepSize: 1,
+      },
+    },
+    y_per: {
+      type: 'linear',
+      display: true,
+      position: 'right',
+      min: 0,
+      max: 100,
+      ticks: {
+        callback: function(value: number) {
+          return value + '%';
+        }
+      },
+      grid: {
+        drawOnChartArea: false,
       },
     },
     x: {
@@ -85,12 +157,32 @@ function getNextStat() {
     .then((result) => {
       if (result.Data.length > 0) {
         const chart = chartInstance.value.chart;
+
         if (chart.data.labels.length >= sampleCount) {
           chart.data.labels = chart.data.labels.slice(1);
           chart.data.datasets[0].data = chart.data.datasets[0].data.slice(1);
+          chart.data.datasets[1].data = chart.data.datasets[1].data.slice(1);
+          chart.data.datasets[2].data = chart.data.datasets[2].data.slice(1);
+          chart.data.datasets[3].data = chart.data.datasets[3].data.slice(1);
+          chart.data.datasets[4].data = chart.data.datasets[4].data.slice(1);
+          chart.data.datasets[5].data = chart.data.datasets[5].data.slice(1);
         }
+
+        const cnt_request = result.Data[0].aggregated.requests;
+        const cnt_hit = result.Data[0].aggregated.hits;
+        const cnt_error = result.Data[0].aggregated.errors;
+        const cnt_origin_offload = result.Data[0].aggregated.origin_offload;
+        const cnt_hit_ratio = (cnt_hit/cnt_request)*100;
+        const cnt_cache_coverage = (cnt_hit/(cnt_hit+cnt_request))*100;
+
         chart.data.labels.push(new Date().valueOf());
-        chart.data.datasets[0].data.push(result.Data[0].aggregated.requests);
+        chart.data.datasets[0].data.push(cnt_request);
+        chart.data.datasets[1].data.push(cnt_hit);
+        chart.data.datasets[2].data.push(cnt_error);
+        chart.data.datasets[3].data.push(cnt_origin_offload);
+        chart.data.datasets[4].data.push(cnt_hit_ratio);
+        chart.data.datasets[5].data.push(cnt_cache_coverage);
+
         chart.update();
       }
     })
